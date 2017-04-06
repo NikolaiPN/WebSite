b034b4dc1caf999949e7c98b34ef0de299f95ea7
NikolaiPN <NikolaiPNikolov@gmail.com>
2017-04-07 01:14:20 +0300

    first commit

---
+++ NikolaiNikolov/Web/Content/Styles/gulpfile.js
@@ -0,0 +1,43 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+//var print = require('gulp-print');
+var cconcat = require('gulp-concat');
+//var autoprefixer = require('gulp-autoprefixer');  // Not in use yet
+var sourcemaps = require('gulp-sourcemaps');
+
+var input = ['**/*.scss', '!node_modules', '!node_modules/**'];
+
+var output = './';
+
+//gulp.task('sass', function () {
+//    return gulp
+//        .src(input)
+//        .pipe(sourcemaps.init())
+//        .pipe(sass({ outputStyle: 'compressed' }))
+//        .pipe(cconcat('Proccessed.min.css'))
+//        .pipe(sourcemaps.write('./maps'))
+//        .pipe(gulp.dest(output));
+//});
+
+gulp.task('sassBig', function () {
+    return gulp
+        .src(input)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(cconcat('Proccessed.css'))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(output));
+});
+
+gulp.task('default', function () {
+    gulp.watch(input, ['sassBig'])
+});
+
+
+//gulp.task('print', function () {
+//    gulp.src(['**/*.scss', '!node_modules', '!node_modules/**'])
+//      .pipe(print(function (filepath) {
+//          return "built: " + filepath;
+//      }))
+//});
+
